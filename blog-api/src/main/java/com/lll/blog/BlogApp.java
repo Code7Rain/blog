d9afa08ec7c53c056ba6cd5af277c4ea506630ab
CoderaQ <2261142446@qq.com>
2022-05-17 21:21:56 +0800

    删除“测试版本控制分支提交一” 再次测试git

--- blog-api/src/main/java/com/lll/blog/BlogApp.java
+++ blog-api/src/main/java/com/lll/blog/BlogApp.java
@@ -5,8 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApp {
-
-//   测试版本控制分支提交一
     public static void main(String[] args) {
         SpringApplication.run(BlogApp.class,args);
     }
